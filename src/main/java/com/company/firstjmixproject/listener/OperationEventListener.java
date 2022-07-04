ffe84a9e075676f5afb060f7513aeb4790450642
Maxim1720 <musiev_maksim@mail.ru>
2022-07-05 02:21:35 +0400

    to replace operationSaving event's logic

--- src/main/java/com/company/firstjmixproject/listener/OperationEventListener.java
+++ src/main/java/com/company/firstjmixproject/listener/OperationEventListener.java
@@ -1,26 +1,27 @@
 package com.company.firstjmixproject.listener;
 
-import com.company.firstjmixproject.entity.Bill;
+import com.company.firstjmixproject.app.exception.FundsValueException;
+import com.company.firstjmixproject.app.service.operation.OperationExecutor;
 import com.company.firstjmixproject.entity.Operation;
-import io.jmix.core.DataManager;
-import io.jmix.core.Id;
 import io.jmix.core.event.EntitySavingEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-
 @Component("idid_OperationEventListener")
 public class OperationEventListener {
     @Autowired
-    private DataManager dataManager;
+    OperationExecutor operationExecutor;
 
     @EventListener
     public void onOperationSaving(EntitySavingEvent<Operation> event) {
-        Bill bill = dataManager.load(Bill.class).id(event.getEntity().getBill().getId()).one();
-        BigDecimal funds = event.getEntity().getBill().getFunds();
-        bill.setFunds(funds.subtract(event.getEntity().getSum()));
-        dataManager.save(bill);
+
+        try {
+            operationExecutor.execute(event.getEntity());
+        }
+        catch (FundsValueException e){
+            log.error(e.getMessage());
+        }
+
     }
 }
