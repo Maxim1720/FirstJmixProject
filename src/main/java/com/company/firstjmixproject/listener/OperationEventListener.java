008a446b2b2bec9bb9cd9d7d70a30d691b8df1f6
Maxim1720 <musiev_maksim@mail.ru>
2022-07-05 02:21:52 +0400

    to add logger

--- src/main/java/com/company/firstjmixproject/listener/OperationEventListener.java
+++ src/main/java/com/company/firstjmixproject/listener/OperationEventListener.java
@@ -4,12 +4,14 @@ import com.company.firstjmixproject.app.exception.FundsValueException;
 import com.company.firstjmixproject.app.service.operation.OperationExecutor;
 import com.company.firstjmixproject.entity.Operation;
 import io.jmix.core.event.EntitySavingEvent;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component("idid_OperationEventListener")
 public class OperationEventListener {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(OperationEventListener.class);
     @Autowired
     OperationExecutor operationExecutor;
 
