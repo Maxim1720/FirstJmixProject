ad32d062b542faeefe2ae75d5d18b360b310bda4
Maxim1720 <musiev_maksim@mail.ru>
2022-07-11 02:52:56 +0400

    to remove empty strings

--- src/main/java/com/company/firstjmixproject/app/exception/AlreadyCanceledOperationException.java
+++ src/main/java/com/company/firstjmixproject/app/exception/AlreadyCanceledOperationException.java
@@ -1,9 +1,7 @@
 package com.company.firstjmixproject.app.exception;
 
 public class AlreadyCanceledOperationException extends RuntimeException{
-
     public AlreadyCanceledOperationException(String msg){
         super(msg);
     }
-
 }
