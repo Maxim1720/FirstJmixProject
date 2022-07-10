1ae5f33c796bc68acb45eaca50932cf12310a861
Maxim1720 <musiev_maksim@mail.ru>
2022-07-11 02:47:48 +0400

    to add transactional annotation on method

--- src/main/java/com/company/firstjmixproject/app/service/operation/OperationExecutor.java
+++ src/main/java/com/company/firstjmixproject/app/service/operation/OperationExecutor.java
@@ -6,6 +6,8 @@ import com.company.firstjmixproject.entity.Operation;
 import io.jmix.core.DataManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
