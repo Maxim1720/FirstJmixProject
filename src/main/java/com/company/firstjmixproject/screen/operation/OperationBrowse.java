fc8958f7621ea2e4ff8f5ab9e85f6960f1b1abf7
Maxim1720 <musiev_maksim@mail.ru>
2022-07-11 02:32:04 +0400

    to add logic of operation cancel

--- src/main/java/com/company/firstjmixproject/screen/operation/OperationBrowse.java
+++ src/main/java/com/company/firstjmixproject/screen/operation/OperationBrowse.java
@@ -1,10 +1,41 @@
 package com.company.firstjmixproject.screen.operation;
 
-import io.jmix.ui.screen.*;
 import com.company.firstjmixproject.entity.Operation;
+import io.jmix.ui.Screens;
+import io.jmix.ui.component.Button;
+import io.jmix.ui.component.Table;
+import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
 
 @UiController("idid_Operation.browse")
 @UiDescriptor("operation-browse.xml")
 @LookupComponent("operationsTable")
 public class OperationBrowse extends StandardLookup<Operation> {
+
+    private Operation selectedOperation;
+    @Autowired
+    private Screens screens;
+    @Autowired
+    private Button cancelOperation;
+
+    @Subscribe("cancelOperation")
+    public void onCancelOperationClick(Button.ClickEvent event) {
+        openCancelEditScreen();
+    }
+
+    private void openCancelEditScreen(){
+        OperationCancel cancelScreen = screens.create(OperationCancel.class);
+        cancelScreen.setEntityToEdit(selectedOperation);
+        cancelScreen.show();
+    }
+
+    @Subscribe("operationsTable")
+    public void onOperationsTableSelection(Table.SelectionEvent<Operation> event) {
+        Optional<Operation> optionalOperation = event.getSelected().stream().findFirst();
+        optionalOperation.ifPresent((op) -> {
+            selectedOperation = op;
+            cancelOperation.setEnabled(!selectedOperation.getIsCanceled());
+        });
+    }
 }
