3022dd3e3efd3df9bcab5e6b6757aa652e4d9d63
Maxim1720 <musiev_maksim@mail.ru>
2022-07-11 02:21:48 +0400

    to init FundsCalculator for delegate handling of funds data

---
+++ src/main/java/com/company/firstjmixproject/app/service/funds/FundsCalculator.java
@@ -0,0 +1,39 @@
+package com.company.firstjmixproject.app.service.funds;
+
+import com.company.firstjmixproject.app.exception.FundsValueException;
+import com.company.firstjmixproject.entity.Bill;
+import com.company.firstjmixproject.entity.Operation;
+import io.jmix.core.Messages;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service("fundsCalculator")
+public class FundsCalculator {
+    @Autowired
+    Messages messages;
+    public BigDecimal calculate(Operation operation, BigDecimal funds){
+
+        switch (operation.getType()) {
+            case ENROLLMENT: {
+                return funds.add(operation.getSum());
+            }
+            case WITHDRAWAL: {
+                funds = funds.subtract(operation.getSum());
+                if(funds.doubleValue() < 0){
+                    throw new FundsValueException(
+                            messages.getMessage(Bill.class,
+                                    Bill.class.getName()+"funds.validation.DecimalMin")
+                    );
+                }
+                return funds;
+            }
+            default:
+                return new BigDecimal(0);
+        }
+
+
+    }
+
+}
