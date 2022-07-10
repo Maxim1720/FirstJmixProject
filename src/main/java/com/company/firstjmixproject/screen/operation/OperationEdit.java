fcb13adc65c2d6634c20bf830731df08e3f59d30
Maxim1720 <musiev_maksim@mail.ru>
2022-07-11 02:39:58 +0400

    to add setSecureFieldsEditable method for set editable of secure fields

--- src/main/java/com/company/firstjmixproject/screen/operation/OperationEdit.java
+++ src/main/java/com/company/firstjmixproject/screen/operation/OperationEdit.java
@@ -3,10 +3,7 @@ package com.company.firstjmixproject.screen.operation;
 import com.company.firstjmixproject.entity.Bill;
 import com.company.firstjmixproject.entity.OperationType;
 import io.jmix.core.EntityStates;
-import io.jmix.ui.component.ComboBox;
-import io.jmix.ui.component.EntityPicker;
-import io.jmix.ui.component.TextField;
-import io.jmix.ui.model.InstanceContainer;
+import io.jmix.ui.component.*;
 import io.jmix.ui.screen.*;
 import com.company.firstjmixproject.entity.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,12 +25,13 @@ public class OperationEdit extends StandardEditor<Operation> {
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
+        setSecureFieldsEditable(entityStates.isNew(getEditedEntity()));
+    }
 
-        boolean canEdit = (operationDc.getItemOrNull() != null);
-
-        billField.setEditable(canEdit);
-        opTypeField.setEditable(canEdit);
-        sumField.setEditable(canEdit);
+    protected void setSecureFieldsEditable(boolean editable){
+        typeField.setEditable(editable);
+        billField.setEditable(editable);
+        sumField.setEditable(editable);
     }
 
     @Autowired
