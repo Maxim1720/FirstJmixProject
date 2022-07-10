5faea3f46181e4c7c897f8337c87b970ac7ab5e0
Maxim1720 <musiev_maksim@mail.ru>
2022-07-11 02:43:07 +0400

    to remove unnecessary log field

--- src/main/java/com/company/firstjmixproject/listener/OperationEventListener.java
+++ src/main/java/com/company/firstjmixproject/listener/OperationEventListener.java
@@ -1,18 +1,15 @@
 package com.company.firstjmixproject.listener;
 
-import com.company.firstjmixproject.app.exception.FundsValueException;
 import com.company.firstjmixproject.app.service.operation.OperationExecutor;
 import com.company.firstjmixproject.entity.Operation;
 import io.jmix.core.DataManager;
 import io.jmix.core.event.EntitySavingEvent;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component("idid_OperationEventListener")
 public class OperationEventListener {
-    private static final Logger log = org.slf4j.LoggerFactory.getLogger(OperationEventListener.class);
     @Autowired
     OperationExecutor operationExecutor;
     @Autowired
