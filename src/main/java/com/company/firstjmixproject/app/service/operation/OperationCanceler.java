43cc49c8304b2e768823e28c61a25580f3d9e7bf
Maxim1720 <musiev_maksim@mail.ru>
2022-07-13 18:32:50 +0400

    to remove unnecessary statements

--- src/main/java/com/company/firstjmixproject/app/service/operation/OperationCanceler.java
+++ src/main/java/com/company/firstjmixproject/app/service/operation/OperationCanceler.java
@@ -5,30 +5,18 @@ import com.company.firstjmixproject.entity.Operation;
 import com.company.firstjmixproject.entity.OperationType;
 import io.jmix.core.DataManager;
 import io.jmix.core.Messages;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
-//@Scope(value="vaadin-ui", proxyMode= ScopedProxyMode.INTERFACES)
 @Transactional
 @Component("idid_OperationCanceler")
 public class OperationCanceler {
 
-    private static final Logger log = LoggerFactory.getLogger(OperationCanceler.class);
     @Autowired
     DataManager dataManager;
-
-    @PersistenceContext
-    EntityManager entityManager;
 
     @Autowired
     private Messages messages;
@@ -45,8 +33,6 @@ public class OperationCanceler {
                 + DateFormat.getDateTimeInstance().format(new Date()));
         op.setType(getCancelOpType(rolledBackOperation.getType()));
         op.setIsCanceled(true);
-
-        log.error(op + " is prepared to roll back operation");
         return op;
     }
 
